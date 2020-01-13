8b728d3f2f423dfca7a171a5e0130bf210e31281
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-13 03:31:55 -0500

    Update Board.php

--- src/Board.php
+++ src/Board.php
@@ -1,1 +1,34 @@
+<?php
 
+namespace Hikura;
+
+/**
+ * The actual game controller.
+ */
+class Board
+{
+    /** @var array $history The current game history. */
+    private $history = [];
+ 
+    /**
+     * Construct a new 4 player chess game.
+     *
+     * @param Helper $helper The helper class.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(Helper $helper)
+    {
+        $this->helper = $helper;
+    }
+    
+    /**
+     * Determine if it RY to move or not.
+     *
+     * @return bool Returns true if it's RY to move and false if not.
+     */
+    public function ryMove(): bool
+    {
+        return $this->helper->turn === 0 || $this->helper->turn === 2;
+    }
+}
