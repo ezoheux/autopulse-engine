2096a204ce2cadf56fd59c43fdfa370674745c46
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-17 14:49:01 -0500

    Add the preloader.

---
+++ helper.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Used to preload the classes.
+ */
+function _preload($preload, string $pattern = "/\.php$/", array $ignore = []): void
+{
+    if (is_array($preload)) {
+        foreach ($preload as $path) {
+            _preload($path, $pattern, $ignore);
+        }
+    } elseif (is_string($preload)) {
+        $path = $preload;
+        if (!in_array($path, $ignore)) {
+            if (is_dir($path)) {
+                if ($dh = opendir($path)) {
+                    while (($file = readdir($dh)) !== false) {
+                        if ($file !== "." && $file !== "..") {
+                            _preload($path . "/" . $file, $pattern, $ignore);
+                        }
+                    }
+                    closedir($dh);
+                }
+            } elseif (is_file($path) && preg_match($pattern, $path)) {
+                if (!opcache_compile_file($path)) {
+                    trigger_error("Preloading Failed", E_USER_ERROR);
+                }
+            }
+        }
+    }
+}
